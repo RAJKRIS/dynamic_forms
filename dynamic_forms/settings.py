8e866ab6b96bbd453c04aec735b30718020a173a
RAJKRIS <rajesh1993krishnan@gmail.com>
2025-03-30 21:51:53 +0530

    fix: update static settings and add gunicorn

--- dynamic_forms/settings.py
+++ dynamic_forms/settings.py
@@ -116,7 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"  # This is required for production
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
